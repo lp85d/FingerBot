28b4a78130ed46481bca29268a1c010bd6a3bf87
Denis Sochi <lp85d@mail.ru>
2024-06-03 22:13:19 +0300

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,53 +1,36 @@
-#ifdef ESP32
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <WebServer.h>
-#else
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
-#endif
-
 #include <WiFiManager.h>
 #include <ArduinoJson.h>
 #include <Servo.h>
 
-#ifdef ESP32
-WebServer server(80);
-#else
 ESP8266WebServer server(80);
 WiFiClient client;
-#endif
-
 Servo servo;
 
-#ifdef ESP32
-const int servoPin = 13; // Для ESP32 используем GPIO13
-#else
-const int servoPin = 0; // Для ESP-01S используем GPIO0
-#endif
-
+const int servoPin = 0;
 String externalIP;
 unsigned long lastUpdateTime = 0;
-const unsigned long updateInterval = 600000; // 10 минут в миллисекундах
+const unsigned long updateInterval = 30000; // Интервал обновления IP (30 секунд)
 unsigned long lastRequestTime = 0;
-const unsigned long requestInterval = 10000; // 10 секунд в миллисекундах
+const unsigned long requestInterval = 6000; // Интервал проверки статуса сервера (6 секунд)
+int currentPosition = 0;
 
 void saveConfigCallback(WiFiManager *myWiFiManager) {
-    // Ваш код, если необходим
+    Serial.println("Entered config mode");
+    Serial.println(WiFi.softAPIP());
+    Serial.println(myWiFiManager->getConfigPortalSSID());
 }
 
 void setup() {
     Serial.begin(115200);
-
     servo.attach(servoPin);
-    servo.write(0); // Устанавливаем начальную позицию серво
+    servo.write(currentPosition);
 
     WiFiManager wifiManager;
     wifiManager.setAPCallback(saveConfigCallback);
-
-    // Настройка WiFiManager для постоянного создания AP в случае неудачи
-    wifiManager.setConfigPortalTimeout(180); // Тайм-аут 3 минуты для конфигурационного портала
+    wifiManager.setConfigPortalTimeout(180);
 
     if (!wifiManager.autoConnect("FingerBot")) {
         Serial.println("Failed to connect and hit timeout");
@@ -55,136 +38,114 @@ void setup() {
         ESP.restart();
     }
 
-    server.on("/", handleRoot); // Регистрация обработчика для корневого пути
+    server.on("/", handleRoot);
     server.begin();
 
-    lastUpdateTime = millis(); // Инициализация lastUpdateTime текущим временем
+    Serial.println("Connected to WiFi network.");
+    Serial.println("Setup complete.");
+
+    lastUpdateTime = millis();
+    lastRequestTime = millis();
 }
 
 void loop() {
     if (WiFi.status() == WL_CONNECTED) {
-        if (millis() - lastUpdateTime > updateInterval) {
+        unsigned long currentMillis = millis();
+
+        if (currentMillis - lastUpdateTime > updateInterval) {
             updateExternalIP();
+            lastUpdateTime = currentMillis;
         }
 
-        if (millis() - lastRequestTime > requestInterval) {
+        if (currentMillis - lastRequestTime > requestInterval) {
             checkServerStatus();
-            lastRequestTime = millis();
+            lastRequestTime = currentMillis;
         }
+    } else {
+        Serial.println("WiFi not connected.");
     }
 
-    server.handleClient(); // Должен вызываться всегда для обработки запросов клиентов
+    server.handleClient();
 }
 
 void handleRoot() {
     Serial.println("handleRoot called");
+
     if (externalIP.isEmpty()) {
         server.send(200, "text/html", "<h1>External IP is not yet available. Please wait...</h1>");
         return;
     }
 
-    String url = "https://fingerbot.ru/wp-json/custom/v1/ip-address?custom_ip_status=" + externalIP;
-    HTTPClient http;
-    #ifdef ESP32
-    http.begin(url);
-    #else
-    http.begin(client, url);
-    #endif
-    http.setTimeout(5000); // Таймаут 5 секунд
-    int httpCode = http.GET();
-
-    Serial.printf("HTTP GET request to %s returned code: %d\n", url.c_str(), httpCode);
-
-    if (httpCode > 0) {
+    String url = "http://fingerbot.ru/wp-json/custom/v1/ip-address?custom_ip_status=" + externalIP;
+    sendHttpRequest(url, [&](int httpCode, const String& payload) {
         if (httpCode == HTTP_CODE_OK) {
-            String payload = http.getString();
-            Serial.println("Response payload: " + payload);
             server.send(200, "application/json", payload);
         } else {
             server.send(500, "text/html", "Failed to get response from server.");
         }
-    } else {
-        server.send(500, "text/html", "Error on HTTP request: " + String(httpCode));
-    }
-
-    http.end();
+    });
 }
 
 void updateExternalIP() {
-    HTTPClient http;
-    #ifdef ESP32
-    http.begin("https://fingerbot.ru/ip/");
-    #else
-    http.begin(client, "https://fingerbot.ru/ip/");
-    #endif
-    http.setTimeout(5000); // Таймаут 5 секунд
-    int httpCode = http.GET();
-    
-    Serial.printf("HTTP GET request to https://fingerbot.ru/ip/ returned code: %d\n", httpCode);
-
-    if (httpCode > 0) {
+    Serial.println("Updating external IP...");
+    sendHttpRequest("http://fingerbot.ru/ip/", [&](int httpCode, const String& payload) {
         if (httpCode == HTTP_CODE_OK) {
-            externalIP = http.getString();
-            lastUpdateTime = millis();
+            externalIP = payload;
             Serial.println("External IP updated: " + externalIP);
+        } else {
+            Serial.printf("Error on HTTP request: %d\n", httpCode);
         }
-    } else {
-        Serial.printf("Error on HTTP request: %d\n", httpCode);
-    }
-    
-    http.end();
+    });
 }
 
 void checkServerStatus() {
-    if (externalIP.isEmpty()) {
-        return;
-    }
+    if (externalIP.isEmpty()) return;
 
-    String url = "https://fingerbot.ru/wp-json/custom/v1/ip-address?custom_ip_status=" + externalIP;
-    HTTPClient http;
-    #ifdef ESP32
-    http.begin(url);
-    #else
-    http.begin(client, url);
-    #endif
-    http.setTimeout(5000); // Таймаут 5 секунд
-    int httpCode = http.GET();
-
-    Serial.printf("HTTP GET request to %s returned code: %d\n", url.c_str(), httpCode);
-
-    if (httpCode > 0) {
+    String url = "http://fingerbot.ru/wp-json/custom/v1/ip-address?custom_ip_status=" + externalIP;
+    sendHttpRequest(url, [&](int httpCode, const String& payload) {
         if (httpCode == HTTP_CODE_OK) {
-            String payload = http.getString();
             handleServerResponse(payload);
+        } else {
+            Serial.printf("Error checking server status. HTTP Code: %d\n", httpCode);
         }
-    } else {
-        Serial.printf("Error on HTTP request: %d\n", httpCode);
-    }
-
-    http.end();
+    });
 }
 
-void handleServerResponse(String payload) {
-    DynamicJsonDocument doc(1024);
-    DeserializationError error = deserializeJson(doc, payload);
-    
+void handleServerResponse(const String& response) {
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, response);
+
     if (error) {
-        Serial.print("deserializeJson() failed: ");
+        Serial.print("Failed to parse response: ");
         Serial.println(error.c_str());
         return;
     }
 
-    if (doc.containsKey("code") && doc["code"] == "no_user_found") {
-        Serial.println("No user found with the specified IP address. Checking again in 10 minutes.");
-        lastUpdateTime = millis(); // Чтобы проверить IP через 10 минут
-        return;
+    int status = doc[0]["custom_ip_status"].as<int>();
+
+    if (status == 1 && currentPosition != 90) {
+        Serial.println("Status 1: Moving servo to 90 degrees.");
+        servo.write(90);
+        currentPosition = 90;
+    } else if (status == 0 && currentPosition != 0) {
+        Serial.println("Status 0: Moving servo to 0 degrees.");
+        servo.write(0);
+        currentPosition = 0;
+    }
+}
+
+void sendHttpRequest(const String& url, std::function<void(int, const String&)> callback) {
+    HTTPClient http;
+    http.begin(client, url); // Изменено на использование WiFiClient
+    http.setTimeout(5000);
+    int httpCode = http.GET();
+    String payload = httpCode > 0 ? http.getString() : "";
+
+    Serial.printf("HTTP GET request to %s returned code: %d\n", url.c_str(), httpCode);
+    if (!payload.isEmpty()) {
+        Serial.println("Response payload: " + payload);
     }
 
-    if (doc[0]["custom_ip_status"] == "1") {
-        Serial.println("Status 1: Moving servo to 90 degrees clockwise.");
-        servo.write(90); // Двигаем сервопривод на 90 градусов по часовой стрелке
-    } else if (doc[0]["custom_ip_status"] == "0") {
-        Serial.println("Status 0: Moving servo to 0 degrees.");
-        servo.write(0); // Двигаем сервопривод обратно в начальное положение
-    }
+    callback(httpCode, payload);
+    http.end();
 }
